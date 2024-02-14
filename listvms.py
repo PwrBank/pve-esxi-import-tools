dc7c06ef5f464c798265bc30d759d11e0aab7add
Wolfgang Bumiller <w.bumiller@proxmox.com>
2024-02-14 16:08:25 +0100

    import listvms.py
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
    Originally-by: Hannes Laimer <h.laimer@proxmox.com>

---
+++ listvms.py
@@ -0,0 +1,81 @@
+from typing import List, Dict, Optional
+import sys
+import json
+import os
+from pyVim.connect import SmartConnectNoSSL, Disconnect
+from pyVmomi import vim
+
+
+def get_datacenter_of_vm(vm: vim.VirtualMachine) -> Optional[vim.Datacenter]:
+    """Find the Datacenter object a VM belongs to."""
+    current = vm.parent
+    while current:
+        if isinstance(current, vim.Datacenter):
+            return current
+        current = current.parent
+    return None
+
+
+def list_vms(service_instance: vim.ServiceInstance) -> List[vim.VirtualMachine]:
+    """List all VMs on the ESXi/vCenter server."""
+    content = service_instance.content
+    vm_view = content.viewManager.CreateContainerView(
+        content.rootFolder,
+        [vim.VirtualMachine],
+        True,
+    )
+    vms = vm_view.view
+    vm_view.Destroy()
+    return vms
+
+
+def get_vm_vmx_info(vm: vim.VirtualMachine) -> Dict[str, str]:
+    """Extract VMX file path and checksum from a VM object."""
+    vmx_path = vm.config.files.vmPathName
+    datastore_name, relative_vmx_path = vmx_path.split('] ', 1)
+    datastore_name = datastore_name.strip('[')
+    return {
+        'datastore': datastore_name,
+        'path': relative_vmx_path,
+        'checksum': vm.config.vmxConfigChecksum.hex() if vm.config.vmxConfigChecksum else 'N/A'
+    }
+
+
+def get_all_datacenters(service_instance: vim.ServiceInstance) -> List[vim.Datacenter]:
+    """Retrieve all datacenters from the ESXi/vCenter server."""
+    content = service_instance.content
+    dc_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.Datacenter], True)
+    datacenters = dc_view.view
+    dc_view.Destroy()
+    return datacenters
+
+
+def main():
+    esxi_host = sys.argv[1]
+    esxi_user = sys.argv[2]
+    esxi_password = os.getenv('ESXI_PASSWORD', '...')
+    si = SmartConnectNoSSL(host=esxi_host, user=esxi_user, pwd=esxi_password)
+
+    try:
+        datacenters = get_all_datacenters(si)
+        vms = list_vms(si)
+        data = {}
+
+        for dc in datacenters:
+            dc_data = {'vms': {}, 'datastores': {}}
+            for vm in vms:
+                if get_datacenter_of_vm(vm) == dc:
+                    vm_info = {'config': get_vm_vmx_info(vm)}
+                    datastore_info = {ds.name: ds.url for ds in vm.config.datastoreUrl}
+                    dc_data['vms'][vm.name] = vm_info
+                    dc_data['datastores'].update(datastore_info)
+
+            data[dc.name] = dc_data
+
+        print(json.dumps(data, indent=2))
+    finally:
+        Disconnect(si)
+
+
+if __name__ == "__main__":
+    main()
