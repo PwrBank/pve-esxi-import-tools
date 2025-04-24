PwrBank/pve-esxi-import-tools

commit 9fee27b19e4cbd7f68adb0056d2fe8545a309700

listvms: add message when skipping vCLS agent VMs
While at it, factor out the checks to make the control flow a little
easier to read and add information why there is an extra check for the
vm.config.

Signed-off-by: Daniel Kral <d.kral@proxmox.com>
Reviewed-by: Max Carrara <m.carrara@proxmox.com>
Link: https://lore.proxmox.com/20250411150634.253684-2-d.kral@proxmox.com

listvms.py
@@ -251,6 +251,20 @@ def fetch_and_update_vm_data(vm: vim.VirtualMachine, data: dict[Any, Any]):
     datastores.update({ds.name: ds.url for ds in vm.config.datastoreUrl})
 
 
+def is_vcls_agent_vm(vm: vim.VirtualMachine) -> bool:
+    # older ESXi installations seem to not expose the vm config
+    if vm.config is not None:
+        return False
+
+    return any(cfg.key == "HDCS.agent"
+               and cfg.value.lower() == "true"
+               for cfg in vm.config.extraConfig)
+
+def is_diskless_vm(vm: vim.VirtualMachine) -> bool:
+    datastore_name, _ = parse_file_path(vm.config.files.vmPathName)
+
+    return not datastore_name
+
 def main():
     args = parse_args()
 
@@ -266,20 +280,12 @@ def main():
         data = {}
         for vm in list_vms(connection):
             # drop vCLS machines
-            vCLS = vm.config is not None and any(
-                cfg.key == "HDCS.agent"
-                and cfg.value.lower() == "true"
-                for cfg in vm.config.extraConfig
-            )
-            if vCLS:
+            if is_vcls_agent_vm(vm):
+                print(f"Skipping vCLS agent VM: {vm.name}", file=sys.stderr)
                 continue
             # drop vms with empty datastore
-            datastore_name, relative_vmx_path = parse_file_path(
-                vm.config.files.vmPathName
-            )
-            if not datastore_name:
-                print(f"Skipping VM (no datastore value): {vm.name}",
-                      file=sys.stderr)
+            if is_diskless_vm(vm):
+                print(f"Skipping diskless VM: {vm.name}", file=sys.stderr)
                 continue
             try:
                 fetch_and_update_vm_data(vm, data)

commit 011c28986d8ee4c4d9b93403d83d0d825fba0c60

list VMs: fix early-return check for config being none
We must early return if it's none, not when it's not.

Fixes: 9fee27b ("listvms: add message when skipping vCLS agent VMs")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

listvms.py
@@ -253,7 +253,7 @@ def fetch_and_update_vm_data(vm: vim.VirtualMachine, data: dict[Any, Any]):
 
 def is_vcls_agent_vm(vm: vim.VirtualMachine) -> bool:
     # older ESXi installations seem to not expose the vm config
-    if vm.config is not None:
+    if vm.config is None:
         return False
 
     return any(cfg.key == "HDCS.agent"